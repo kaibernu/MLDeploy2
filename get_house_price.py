47ab489c928f06967b25dd79d3bd8b427b46b00d
Kailash Bernucci <kaibernu@gmail.com>
2020-05-29 15:55:36 -0300

    Add files via upload

---
+++ get_house_price.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import  LinearRegression
+from  sklearn.model_selection import  train_test_split
+
+import time
+
+data = pd.read_csv('/Users/kaka/Desktop/Curso_Deploying_Machine_Learning_Models/House_Prices_Streamlit/data.csv')
+
+st.title('Calcule o Preço do Imóvel - Boston/EUA')
+
+show_data = st.checkbox('Veja o Dataset')
+
+if show_data:
+    st.write(data.head())
+
+
+sqft = st.slider("Metragem (Square Feet):",int(data.sqft_living.min()),int(data.sqft_living.max()),int(data.sqft_living.mean()))
+
+bath = st.slider("No. de Banheiros :",int(data.bathrooms.min()),int(data.bathrooms.max()),int(data.bathrooms.mean()))
+bed = st.slider("No. de Quartos :",int(data.bedrooms.min()),int(data.bedrooms.max()),int(data.bedrooms.mean()))
+floor = st.slider("No. de Andares:",int(data.floors.min()),int(data.floors.max()),int(data.floors.mean()))
+
+
+X = data.drop('price',axis=1)
+y = data.price
+
+model = LinearRegression()
+
+model.fit(X,y)
+
+pred = model.predict([[sqft,bath,bed,floor]])[0]
+
+
+
+if st.button("Gerar Preço do Imóvel"):
+    st.header("Preço Estimado do Imóvel em USD {}".format(int(pred)))
+    
